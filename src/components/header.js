b56da30df495253537f7eca4e2ec4343f0b69815
Sean Mc Ternan <sean@beyondthesummitpdd.com>
2019-10-08 17:32:51 -0600

    Update style on site pages

--- src/components/header.js
+++ src/components/header.js
@@ -1,16 +1,21 @@
 import React from 'react'
 import { Link } from 'gatsby'
+import headerStyles from './header.module.scss'
 
 const Header = () => {
     return (
-        <header>
-            <h1>Sean Mc Ternan's Portfolio Site</h1>
+        <header className={headerStyles.header}>
+            <h1>
+                <Link className={headerStyles.title} to="/">
+                    Sean Mc Ternan
+                </Link>
+            </h1>
             <nav>
-            <ul>
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="/about">About</Link></li>
-            <li><Link to="/contact">Contact</Link></li>
-            <li><Link to="/blog">Blog</Link></li>
+            <ul className={headerStyles.navList}>
+            <li><Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to="/">Home</Link></li>
+            <li><Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to="/about">About</Link></li>
+            <li><Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to="/contact">Contact</Link></li>
+            <li><Link className={headerStyles.navItem} activeClassName={headerStyles.activeNavItem} to="/blog">Blog</Link></li>
             </ul>
             </nav>
         </header>
@@ -18,3 +23,4 @@ const Header = () => {
 }
 
 export default Header
+
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import Header from './header'
 import Footer from './footer'
+import layoutSyles from './layout.module.scss'
+import '../styles/index.scss'
 
 const Layout = (props) => {
     return (
-        <div>
-        <Header/>
-           {props.children}
+        <div className={layoutSyles.container}>
+        <div className={layoutSyles.content}>
+            <Header/>
+            {props.children}
+        </div>
         <Footer/>
         </div>
     )
