4d9346df308bbefb08885e36b13677198a639e95
Sean Mc Ternan <sean@beyondthesummitpdd.com>
2019-10-13 11:35:35 -0600

    Modified environment variables and home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,4 @@
 import React from "react"
-import { Link } from 'gatsby'
 import Layout from '../components/layout'
 import Head from '../components/head'
 
@@ -9,7 +8,6 @@ const IndexPage = () => {
         <Head title="Home"/>
         <h1>Hello</h1>
         <h2>I'm Sean, a developer in training living in Fernie BC</h2>
-        <p>Need a developer? <Link to="/contact">Contact Me</Link></p>
         </Layout>
     )
 }
